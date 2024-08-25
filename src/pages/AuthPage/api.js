2cb314a2fd0ea19002fe66fe8724503eac48f48d
imranaxix <imranfreelancer69@gmail.com>
2024-08-25 19:36:41 +0500

    fix try1

--- src/pages/AuthPage/api.js
+++ src/pages/AuthPage/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const loginRest = async (username, secret) => {
   return await axios.get("https://api.chatengine.io/users/me/", {
     headers: {
-      "Project-ID": "e6ad633b-e74c-4261-aeed-edb5b476993f",  // Hardcoded value
+      "Project-ID": VITE_CHAT_ENGINE_PROJECT_ID, 
       "User-Name": username,
       "User-Secret": secret,
     },
@@ -16,7 +16,7 @@ const signupRest = async (username, secret, email, first_name, last_name) => {
     { username, secret, email, first_name, last_name },
     {
       headers: {
-        "Private-Key": "5b03383f-d370-4d15-a559-4101c9005dfa"  // Hardcoded value
+        "Private-Key": VITE_CHAT_ENGINE_PRIVATE_KEY
       }
     }
   );
